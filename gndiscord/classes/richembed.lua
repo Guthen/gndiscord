20a94761c53c86cea0db53ed6ce8652a3cfb6d06
Guthen <33220603+Guthen@users.noreply.github.com>
2019-12-24 00:17:24 +0100

    RE : new

--- gndiscord/classes/richembed.lua
+++ gndiscord/classes/richembed.lua
@@ -1,7 +1,14 @@
 local RichEmbed = {}
 
 function RichEmbed:new( tbl )
-    return setmetatable( tbl or {}, { __index = RichEmbed } )
+    local embed = tbl or {}
+        embed.fields = embed.fields or {}
+        embed.author = embed.author or {}
+        embed.footer = embed.footer or {}
+        embed.image = embed.image or {}
+        embed.thumbnail = embed.thumbnail or {}
+
+    return setmetatable( embed, { __index = RichEmbed } )
 end
 
 return RichEmbed
