f5f1895892f0cca106f7810466e4bb0b616e9334
Guthen <33220603+Guthen@users.noreply.github.com>
2019-12-24 00:12:21 +0100

    Update richembed.lua

--- gndiscord/classes/richembed.lua
+++ gndiscord/classes/richembed.lua
@@ -1,14 +1,7 @@
 local RichEmbed = {}
 
-local function set( tbl, key, value )
-    tbl[key] = tbl[key] or value
-end
-
-function RichEmbed:new( data )
-    local embed = data or {}
-        set( embed, "title", "Title" )
-        set( embed, "description", "Description" )
-
+function RichEmbed:new( tbl )
+    return setmetatable( tbl or {}, { __index = RichEmbed } )
 end
 
 return RichEmbed
